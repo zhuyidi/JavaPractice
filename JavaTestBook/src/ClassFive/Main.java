b99e4691de5844175e1e8493411104f95083e536
zhuyidi <1311243087@qq.com>
2017-12-26 23:13:23 +0800

    update

---
+++ JavaTestBook/src/ClassFive/Main.java
@@ -0,0 +1,20 @@
+package ClassFive;
+
+
+import java.io.*;
+
+/**
+ * Created by dela on 12/26/17.
+ */
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+//        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+//
+
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(new );
+        String str = bufferedReader.readLine();
+        System.out.println("accept:" + str);
+        System.out.println("totally input " + str.length() + " characters.");
+    }
+}
